ff915f78f8b4defcd1cabb53dbd8c46054bd1a9b
anivillegas26 <anivillegas26@gmail.com>
2025-05-22 23:26:50 -0400

    01-variables

---
+++ holamundo.py
@@ -0,0 +1,24 @@
+print ("Hola Mundo!!!")
+
+nombre = "ana"
+apellido = "villegas"
+
+# UTILIZANDO VARIABLES EN UN PRINT CON COMAS
+print ("Hola mi nombre es ", nombre, "y mi apellido es " , apellido )
+
+# IMPRIMIENDO CON OPERADOR DE CONCATENACION
+print ("Hola mi nombre es " + nombre + " y mi apellido es " + apellido  )
+
+# IMPRIMIENDO CON F-STRINGS (CADENAS LITERALES)
+print(f"Hola mi nombre es {nombre} y mi apellido es {apellido} ")
+
+# INICIANDO MULTI VARIABLES EN UNA SOLA LINEA (NO RECOMENDADO)
+ciudad, region, pais = "Castro", "Los Lagos", "Chile"
+print (f"Hola soy de {ciudad}, {region}, {pais}")
+
+peso = 75
+edad = 31
+
+
+
+ 
