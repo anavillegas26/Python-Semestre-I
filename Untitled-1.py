adc578fb18b92412729c3f150795f3d064caba9f
anivillegas26 <anivillegas26@gmail.com>
2025-05-25 22:46:46 -0400

    actualizacion de informacion

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,42 @@
 
+#INICIANDO VARIABLE NUMERICAS
+edad = 31
+estatura = 1.71
+peso = 75.4
+complejo = 2 + 9j # INICIANDO UN NUMERO COMPLEJO
+complejo2 = complex (1,4)
+
+#IMPRESION NUMEROS COMPLEJOS
+print (complejo)
+print (complejo2)
+
+#operacion aritmetica
+imc = peso / (estatura **2)
+print (f"su IMC es: {imc}")
+
+#FORMATO DE SALIDA DEL RESULTADO (UTILIZANDO F- STRINGS Y :2f)
+print(f"Su IMC es:{imc:.2f}")# salida con 2 decimales
+
+#FORMATO DE SALIDA DEL RESULTADO (UTILIZANDOEL METODO FORMAT)
+print ("Su IMC es: {:.2f}". format(imc))
+
+#TRANDFORMANDO UN NUMERO ENTERO EN UN NUMERO FLOTANTE
+print(float(edad))
+
+#04 - DATOS DE TIPO STRING (CADENAS DE TEXTO)
+carrera = 'Ingeniería Civil en Informática'
+asignatura = "Programación"
+descripcion = '''Esta es una asignatura
+de primer semestre '''
+
+#APLICANDO EL METODO SPLIT (GENERA UN ARREGLO DE CADENA)
+print(descripcion.split())
+
+#ARREGLO NUMERICO
+
+
+
+
+
+
+ 
--- holamundo.py
+++ holamundo.py
@@ -16,8 +16,30 @@ print(f"Hola mi nombre es {nombre} y mi apellido es {apellido} ")
 ciudad, region, pais = "Castro", "Los Lagos", "Chile"
 print (f"Hola soy de {ciudad}, {region}, {pais}")
 
-peso = 75
+# 01 Iniciando variables numericas
 edad = 31
+estatura = 1.71
+peso = 75.4
+complejo = 2 + 9j # INICIANDO UN NUMERO COMPLEJO
+complejo2 = complex (1,4)
+
+#IMPRESION NUMEROS COMPLEJOS
+print (complejo)
+print (complejo2)
+
+#operacion aritmetica
+imc = peso / (estatura **2)
+print (f"su IMC es: {imc}")
+
+#FORMATO DE SALIDA DEL RESULTADO (UTILIZANDO F- STRINGS Y :2f)
+print(f"Su IMC es:{imc:.2f}")# salida con 2 decimales
+
+#FORMATO DE SALIDA DEL RESULTADO (UTILIZANDOEL METODO FORMAT)
+print ("Su IMC es: {:.2f}". format(imc))
+
+
+
+
 
 
 
