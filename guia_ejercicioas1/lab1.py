dcb37ef7271e9801064d492ebd5eac8dd30d64e2
anivillegas26 <anivillegas26@gmail.com>
2025-06-04 14:01:41 -0400

    actualizando_lab1

---
+++ guia_ejercicioas1/lab1.py
@@ -0,0 +1,24 @@
+#solicitar cantidades de herramientas
+martillos = int(input("Por favor ingresar la cantidad de martillos: "))
+clavos = int(input("Por favor ingresar la  cantidad de clavos: "))
+serrucho = int(input("Por favor ingresar la cantiadad de serruchos: "))
+tornillos = int(input("Por favor ingresar la cantidad de tornillos: "))
+
+valor_martillo = float(input("Por favor ingresar unitario el valor del martillo : "))
+valor_clavos = float(input("Por favor ingresar unitario el valor de los clavos: ")) 
+valor_serrucho = float(input("Por favor ingresar unitario el valor del serrucho: "))
+valor_tornillos =  float(input("Por favor ingresar unitario el valor del martillo: "))
+
+total_martillo = martillos * valor_martillo
+print(f"El  valor total de los martillos es : {total_martillo:.2f}")
+total_clavos = clavos * valor_clavos
+print(f"El valor toral de los clavos es. {total_clavos:.2f}")
+total_serrucho = serrucho * valor_serrucho
+print(f"El valor total de los serruchos es: {total_serrucho:.2f} ")
+total_tornillos = tornillos * valor_tornillos
+print(f"El valor total de los tornillos  es: {total_tornillos:.2f} ")
+total_subtotales = total_serrucho + total_tornillos + total_clavos + total_serrucho
+valor_max = max(total_serrucho, total_tornillos, total_clavos, total_serrucho) 
+print(f"El valor maximo de todos los productos es: {valor_max}")
+valor_min = min(total_serrucho, total_tornillos, total_clavos, total_serrucho) 
+print(f"El valor minimo de total de productos es : {valor_min}")
